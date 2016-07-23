ffb1eaabfeedee912e5ebf42aa8962b23e931504
yasar-salim <yasarsalim@bytekat.com>
2016-07-23 17:49:56 +0530

    modified name

--- yasql.inc.php
+++ yasql.inc.php
@@ -5,14 +5,14 @@
 //Current Version   :      0.9
 
 
-define('SERVER',"localhost"); 
-define('USERNAME',"root");   
-define('PASSWORD',"");       
-define('DATABASE',"bkat");   
+define('SERVER',"localhost");
+define('USERNAME',"root");
+define('PASSWORD',"");
+define('DATABASE',"bkat");
 
 
 
-class yqueries
+class yasql
 {
   public function select_count($column,$table,$condition){
     $sql="select  max($column) from $table where $condition";
@@ -31,43 +31,43 @@ class yqueries
     $query=new response();
     return $query->select_math_queries($sql);
   }
-  
+
   public function select_min($column,$table,$condition) {
     $sql="select  min($column) from $table where $condition";
     $query=new response();
     return $query->select_math_queries($sql);
   }
 
-  public function select_max($column,$table,$condition) {    
+  public function select_max($column,$table,$condition) {
     $sql="select  max($column) from $table where $condition";
     $query=new response();
     return $query->select_math_queries($sql);
   }
-  
+
   public function select_rows($column,$table,$condition){
       $sql="select  $column from $table where $condition";
       $query=new response();
       return $query->select_queries($sql);
   }
-  
+
   public function delete($table,$condition){
       $sql="delete from $table where $condition";
       $query=new response();
       return $query->update_queries($sql);
   }
-  
+
   public function update($table,$updation,$condition){
       $sql="update $table set $updation where $condition";
       $query=new response();
       return $query->update_queries($sql);
   }
-  
+
   public function insert($table,$values){
       $sql="insert into $table set $values";
       $query=new response();
       return $query->update_queries($sql);
   }
-  
+
 }
 
 
@@ -89,10 +89,10 @@ class response
         }
         else {
             echo $conn->connect_error;
-        } 
+        }
         $conn->close();
     }
-    
+
     public function select_queries($query){
         $db=new db();
         $conn=$db->connect();
@@ -109,10 +109,10 @@ class response
         }
         else {
             echo $conn->connect_error;
-        } 
+        }
         $conn->close();
     }
-    
+
     public function update_queries($query){
         $db=new db();
         $conn=$db->connect();
@@ -127,9 +127,9 @@ class response
         }
         else {
             echo $conn->connect_error;
-        } 
+        }
         $conn->close();
-    }    
+    }
 }
 
 class db
